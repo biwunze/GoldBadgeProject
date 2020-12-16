13e5e08bd07a84969409c63631290cfa1113c2d9
Weston09! <biwunze@gmail.com>
2020-12-15 20:11:22 -0500

    With test

---
+++ GoldBadge_01_Test/MealContentRepoTests.cs
@@ -0,0 +1,89 @@
+﻿using GoldBadge_01_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace GoldBadge_01_Test
+{
+    [TestClass]
+    public class MealContentRepoTests
+    {
+        private MealContentRepo _repo;
+        private MealContent _mealCont;
+
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new MealContentRepo();
+            _mealCont = new MealContent(5, "Turkey Breakfast", "Toast, turkey sausage, egg whites, wheat toast, and Milk.", 9, MealType.Breakfast);
+
+            _repo.AddMealToMenuList(_mealCont);
+        }
+
+        //Add Method
+        [TestMethod]
+        public void AddToMenu_ShouldGetNotNull()
+        {
+            // Arrange = Setting up the playing field
+            MealContent meal = new MealContent();
+            meal.MealNumber = 7;
+            MealContentRepo repo = new MealContentRepo();
+
+            // Act = Get/run the code we want to test
+            repo.AddMealToMenuList(meal);
+            MealContent mealFromMenu = repo.GetMealByMealNumber(7);
+
+            // Assert = Use the assert class to verify the expected outcome
+            Assert.IsNotNull(mealFromMenu);
+        }
+
+        [TestMethod]
+        public void GetMenuList_NotBeNull()
+        {
+            // Arrange
+            MealContentRepo mealTestRepo = new MealContentRepo();
+            //MealContent mealTestCont = new MealContent();
+
+            // Act
+            List<MealContent> listFromTestRepo = mealTestRepo.GetMenuList();
+
+            // Assert
+            Assert.IsNotNull(listFromTestRepo);
+
+            //_repo.GetMenuList(_mealCont);
+            //_repo.GetMenuList(_mealCont);
+            //_repo.GetMenuList();
+            //MealContentRepo getList = _repo.GetMenuList();
+            //return 
+
+
+        }
+
+        [TestMethod]
+        public void DeleteMeal_True()
+        {
+            // Arrange
+
+            // Act
+            bool deleteThisTest = _repo.DeleteMeal(_mealCont.MealNumber);
+
+            // Assert
+            Assert.IsTrue(deleteThisTest);
+        }
+
+        [TestMethod]
+        public void TestForGetMealByNum()
+        {
+            //Arrange
+            MealContentRepo mealTestRepo = new MealContentRepo();
+            MealContent mealTestCont = new MealContent();
+            mealTestRepo.AddMealToMenuList(mealTestCont);
+
+            //Act
+            MealContent mealByNum = mealTestRepo.GetMealByMealNumber(mealTestCont.MealNumber);
+
+            //Assert
+            Assert.AreEqual(mealTestCont, mealByNum);
+        }
+    }
+}
---
+++ GoldBadge_01_Test/MealContentTests.cs
@@ -0,0 +1,23 @@
+﻿using GoldBadge_01_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GoldBadge_01_Test
+{
+    [TestClass]
+    public class MealContentTests
+    {
+        [TestMethod]
+        public void SetMealNumber_ShouldSetCorrectNum()
+        {
+            MealContent content = new MealContent();                    //Testing propery
+
+            content.MealName = "Large Fiesta Salad Combo";
+
+            string expected = "Large Fiesta Salad Combo";
+            string actual = content.MealName;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
